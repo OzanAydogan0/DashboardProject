d7297c7b5326ea848a04cd15106778fb6dcd23ad
Ozan AYDOĞAN <ozan.aydogan@htr.local>
2026-07-23 09:34:52 +0300

    project deteils içi daha bi tatlış oldu3

--- backend/dashboardapi/obj/Debug/net10.0/dashboardapi.AssemblyInfo.cs
+++ backend/dashboardapi/obj/Debug/net10.0/dashboardapi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dashboardapi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb4f905660f16ea1b7a028a2f9838ab19ff7158a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d898c721d8deb027ca34ffcbbb09541a1a30aa03")]
 [assembly: System.Reflection.AssemblyProductAttribute("dashboardapi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dashboardapi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
